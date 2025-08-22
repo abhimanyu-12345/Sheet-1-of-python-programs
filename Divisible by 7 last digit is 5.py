8d4470a0054684bc9751ab82ed99677aaa94aa31
abhimanyu-12345 <abhimanyu.singh_bca24@gla.ac.in>
2025-08-22 19:42:13 +0530

    Update Divisible by 7 last digit is 5.py

--- Divisible by 7 last digit is 5.py
+++ Divisible by 7 last digit is 5.py
@@ -1,6 +1,6 @@
-num = int(input("Enter a positive number: "))  
-
-if num % 7 == 0 and num % 10 == 5:
-    print("Yes, the number is divisible by 7 and the last digit is 5")
+n = int(input("Enter a positive number: "))  
+if n % 7 == 0 and n % 10 == 5:
+    print(" the number is divisible by 7 and the last digit is 5")
 else:
-    print("No, the number does not meet both the conditions")
+    print("the number does not both the conditions")
+
